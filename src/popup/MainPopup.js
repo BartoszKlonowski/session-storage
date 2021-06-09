eb5792c1f2b8d1b8a9994a8a608a393c056325cb
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-06-09 21:22:25 +0200

    Popup - basic placeholder implementation (#2)
    * Add icon of the project and extension
    * Define initial and basic browser actions
    * Implement empty Popup as placeholder

---
+++ src/popup/MainPopup.js
@@ -0,0 +1,27 @@
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+    });
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs.executeScript({ file: "/content_scripts/beastify.js" })
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
