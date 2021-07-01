3f8a825deeee08902e5373008c4fed24ec179e2c
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-07-01 23:19:01 +0200

    Use className for click target recognition

--- app/src/popup/MainPopup.js
+++ app/src/popup/MainPopup.js
@@ -1,15 +1,8 @@
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
 function listenForClicks() {
     document.addEventListener("click", () => {});
 }
 
-/**
- * There was an error executing the script.
- * Display the popup's error message, and hide the normal UI.
- */
+
 function reportExecuteScriptError(error) {
     document.querySelector("#popup-content").classList.add("hidden");
     document.querySelector("#error-content").classList.remove("hidden");
