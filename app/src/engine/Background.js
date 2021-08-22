3d11b40c93c857c1d33363bef6258476faa6bf84
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-08-22 03:43:55 +0200

    Create the initial database when plugin loads
    The `window.onload` opens the database with the initial version 1 and
    performs the setup if it is not yet created in the storage.
    Global `window` is used so that the created db will be common for all
    tabs and will be persisted for a launching device.
    
    The created schema is:
    ```
    {
      sessionName: VARCHAR NOT UNIQUE,
      sessionObject: VARCHAR NOT UNIQUE,
      id: INT NOT NULL,
      PRIMARY KEY (id),
    }
    ```

--- app/src/engine/Background.js
+++ app/src/engine/Background.js
@@ -1,5 +1,29 @@
 import Engine from "./Engine";
 
+let databaseHandler;
+
+window.onload = () => {
+    let request = window.indexedDB.open("sessionDB", 1);
+    request.onerror = () => {
+        browser.notifications.create({
+            type: "basic",
+            iconUrl: "",
+            title: `ERROR!`,
+            message: `Could not load database IDB`,
+        });
+    };
+    request.onsuccess = () => {
+        databaseHandler = request.result;
+    };
+    request.onupgradeneeded = (arg) => {
+        if (arg !== null) {
+            databaseHandler = request.result.createObjectStore("sessionDB", {keyPath: "id", autoIncrement: true});
+            databaseHandler.createIndex("sessionName", "sessionName", {unique: false});
+            databaseHandler.createIndex("sessionObject", "sessionObject", {unique: false});
+        }
+    };
+};
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const engine = new Engine(browser);
     engine
