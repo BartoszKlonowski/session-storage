3927d4ddcf1c706892df84c909e4dfdc802b174b
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-10-24 19:53:10 +0200

    Place all layout implementation in `<MainPopup/>`
    The `<MainPopup/>` element is responsible to render the root of the
    popup layout. It currently holds all the legacy HTML implementation.
    This has been tested the manually by building it and performing the
    "SAVE" action.
    
    NOTE: Both scripts loading React were defaulted to the production mode
    so that each time the `install` script is launched, the production-ready
    build will be prepared. It does not affect the development speed, but
    makes the deployment easier.

--- app/src/popup/MainPopup.js
+++ app/src/popup/MainPopup.js
@@ -1,3 +1,36 @@
 import * as logic from "./MainPopupLogic";
+import * as React from 'react';
+import ReactDOM from "react-dom";
+
+export class MainPopup extends React.Component {
+    render() {
+        return(
+            <>
+            <div id="popup-content">
+
+            <div class="panel-plugin-name">
+              <a href="https://github.com/BartoszKlonowski/keep-your-session">Keep Your Session</a>
+            </div>
+      
+            <form id="mainForm">
+              <div class="panel-session-name">
+                <input id="sessionNameInput" list="sessions" type="text" placeholder="..." autofocus/>
+                <datalist id="sessions"></datalist>
+              </div>
+              <div class="panel-actions">
+                <button type="submit" id="saveButton"><i class="glyphicon glyphicon-floppy-disk"></i></button>
+                <button type="submit" id="deleteButton"><i class="glyphicon glyphicon-trash"></i></button>
+                <button type="submit" id="reopenButton"><i class="glyphicon glyphicon-refresh"></i></button>
+              </div>
+            </form>
+          </div>
+          </>
+        )
+    }
+}
 
 logic.listenForClicks(document, browser);
+ReactDOM.render(
+    <MainPopup />,
+    document.getElementById('root')
+);
