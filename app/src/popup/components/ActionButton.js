6f79d42d7dd2ebd58e64eabd125bb811d4da9cb3
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-11-02 03:25:20 +0100

    Disable prop-types rule for React components

--- app/src/popup/components/ActionButton.js
+++ app/src/popup/components/ActionButton.js
@@ -3,8 +3,7 @@ import React from "react";
 export class ActionButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
     render() {
