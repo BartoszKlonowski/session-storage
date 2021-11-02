6a0cf6eacbf5b5c110351aa464fabc1a245ed866
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-11-02 03:15:50 +0100

    Implement ActionButton to use props

--- app/src/popup/components/ActionButton.js
+++ app/src/popup/components/ActionButton.js
@@ -3,11 +3,18 @@ import React from "react";
 export class ActionButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+        };
     }
 
     render() {
-        return <></>;
+        return (
+            <>
+                <button type="submit" title={this.props.text} id={this.props.name}>
+                    <i className={this.props.icon}></i>
+                </button>
+            </>
+        );
     }
 }
 
