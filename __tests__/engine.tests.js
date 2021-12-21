1e6ae3976ace5e9dae43435ef3b0876339d38dee
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2021-12-21 21:37:44 +0100

    fix ESLint remarks

--- __tests__/engine.tests.js
+++ __tests__/engine.tests.js
@@ -77,34 +77,34 @@ test("All tabs should have window ID replaced with current and correct one", () 
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 1
+            windowId: 1,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 3
+            windowId: 3,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 15
+            windowId: 15,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 8
-        }
+            windowId: 8,
+        },
     ];
 
     const correctWindowId = 1;
     engine.assignTabsToCurrentWindow(tabs, correctWindowId);
-    for(let tab of tabs) {
+    for (let tab of tabs) {
         expect(tab.windowId).toBe(correctWindowId);
     }
 });
@@ -117,34 +117,34 @@ test("All tabs should have window ID replaced with default current window ID for
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 1
+            windowId: 1,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 3
+            windowId: 3,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 15
+            windowId: 15,
         },
         {
             active: true,
             index: 6,
             pinned: false,
             url: "about:debugging#/runtime/this-firefox",
-            windowId: 8
-        }
+            windowId: 8,
+        },
     ];
 
     const WINDOW_ID_CURRENT = -2;
     engine.assignTabsToCurrentWindow(tabs, "incorrect window's ID", WINDOW_ID_CURRENT);
-    for(let tab of tabs) {
+    for (let tab of tabs) {
         expect(tab.windowId).toBe(WINDOW_ID_CURRENT);
     }
 });
