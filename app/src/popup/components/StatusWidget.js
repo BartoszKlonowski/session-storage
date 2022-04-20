67dbefbfce50904395301c791887f52a3c4cbffc
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-04-20 20:21:31 +0200

    Add empty StatusWidget component

---
+++ app/src/popup/components/StatusWidget.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class StatusWidget extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <>
+            </>
+        );
+    }
+}
