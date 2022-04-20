83b91e6cc00570bde6263755a41e7b9d9bdad929
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-04-20 20:37:55 +0200

    Render the status widget in main popup

--- app/src/popup/MainPopup.js
+++ app/src/popup/MainPopup.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import ExpandedSessionListInput from "./components/ExpandedSessionListInput";
 import ActionButton from "./components/ActionButton";
 import Logo from "./components/Logo";
+import { StatusWidget } from "./components/StatusWidget";
 
 export class MainPopup extends React.Component {
     constructor(props) {
@@ -23,6 +24,7 @@ export class MainPopup extends React.Component {
                     <ActionButton name="deleteButton" text="DELETE" icon="glyphicon glyphicon-trash" />
                     <ActionButton name="reopenButton" text="REOPEN" icon="glyphicon glyphicon-refresh" />
                 </div>
+                <StatusWidget/>
             </form>
         );
     }
