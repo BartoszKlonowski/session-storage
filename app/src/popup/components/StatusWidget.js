b359a80045642c1081f16b1c1559d854d4ed00be
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-04-23 16:13:31 +0200

    Use CSSTransition in StatusWidget

--- app/src/popup/components/StatusWidget.js
+++ app/src/popup/components/StatusWidget.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
 
 export class StatusWidget extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            status: "none",
+        }
+    }
+
+    componentDidMount() {
+        console.log("status is: ", this.props.status);
+        this.setState({status: this.props.status});
     }
 
     render() {
         return(
-            <div className={"status-widget"}>
-            </div>
+            <CSSTransition in={this.props.status === "success"} timeout={2000} classNames={"status-widget"}>
+                <div className={"status-widget"}>
+                </div>
+            </CSSTransition>
         );
     }
 }
