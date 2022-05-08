16355b5dd19293979902b54044e224ab9c6d7f9e
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2022-05-08 14:48:15 +0200

    Implement basic reusable button

---
+++ app/src/popup/components/reusable/Button.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    renderVariantButton() {
+        return (
+            <button type="submit" className={`${this.props.buttonType}`} title={this.props.text} id={this.props.name}>
+                {this.props.children}
+            </button>
+        );
+    }
+
+    renderDefaultButton() {
+        return (
+            <button type="submit" title={this.props.text} id={this.props.name}>
+                {this.props.children}
+            </button>
+        );
+    }
+
+    render() {
+        if (this.props.buttonType) {
+            return this.renderVariantButton();
+        } else {
+            return this.renderDefaultButton();
+        }
+    }
+}
+
+export default ActionButton;
