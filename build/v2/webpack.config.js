40d0202f685d6aefda80700e2fa778f6d14b9c4f
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-03-24 23:21:11 +0100

    Introduce separate build v2/v3 directories

---
+++ build/v2/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        "src/popup/MainPopup": "./build/v2/app/src/popup/MainPopup.js",
+        "src/engine/Background": "./build/v2/app/src/engine/Background.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "build/v2/app"),
+    },
+};
---
+++ build/v3/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        "src/popup/MainPopup": "./build/v3/app/src/popup/MainPopup.js",
+        "src/engine/Background": "./build/v3/app/src/engine/Background.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "build/v3/app"),
+    },
+};
