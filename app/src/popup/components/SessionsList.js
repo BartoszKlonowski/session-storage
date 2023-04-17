943caca92f17d1fca5da4029c489071f2553e6e8
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-04-18 01:02:08 +0200

    Adjust the styling, layout and designs

--- app/src/popup/components/SessionsList.js
+++ app/src/popup/components/SessionsList.js
@@ -27,7 +27,9 @@ const SessionTile = ({name, onSelect}) => {
 
 export const SessionsList = ({sessions, selectedSession, onSelect}) => {
     if (!sessions?.length) {
-        return null;
+        return (
+            <div className="sessions-list-container"/>
+        );
     }
 
     const selectedSessions = sessions.filter((session) => session.includes(selectedSession));
