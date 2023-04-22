a1fc08f87ae4f6d7c20199bf16858574e7e97183
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-04-22 22:06:49 +0200

    Test the i18n implementation

--- __tests__/setupTest.js
+++ __tests__/setupTest.js
@@ -1,5 +1,7 @@
 if (!global.browser) global.browser = {tabs: {}};
 if (!browser) var browser = {};
+if (!global.browser.i18n) global.browser = {tabs: {}, i18n: {}};
+if (!global.browser.i18n.getUILanguage) global.browser = {tabs: {}, i18n: {getUILanguage: () => "en"}};
 
 test("Browser is defined correctly for web extension testing", () => {
     expect(browser).toBeDefined();
