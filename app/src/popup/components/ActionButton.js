7690c76c1d9a281d50265a2cef6ef5a056ff3a93
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-04-22 22:08:34 +0200

    Utilize the translation component in the application

--- app/src/popup/components/ActionButton.js
+++ app/src/popup/components/ActionButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {translate} from "../../engine/i18n";
 import Button from "./reusable/Button";
 
 export class ActionButton extends React.Component {
@@ -11,7 +12,7 @@ export class ActionButton extends React.Component {
         return (
             <Button text={this.props.text} name={this.props.name}>
                 <i id={this.props.name} className={this.props.icon}></i>
-                {this.props.text}
+                {translate(this.props.text.toLowerCase())}
             </Button>
         );
     }
