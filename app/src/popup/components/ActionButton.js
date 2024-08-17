44901e14e2bab48fe9ebbf28a39842868e7da8c3
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2024-08-18 01:50:49 +0200

    Fix the issue of missing text in button

--- app/src/popup/components/ActionButton.js
+++ app/src/popup/components/ActionButton.js
@@ -5,16 +5,13 @@ import Button from "./reusable/Button";
 export class ActionButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            translatedButtonText: translate(this.props.text.toLowerCase()),
-        };
     }
 
     render() {
         return (
-            <Button text={this.state.translatedButtonText} name={this.props.name}>
+            <Button text={translate(this.props.text.toLowerCase())} name={this.props.name}>
                 <i id={this.props.name} className={this.props.icon}></i>
-                {this.state.translatedButtonText}
+                {translate(this.props.text.toLowerCase())}
             </Button>
         );
     }
