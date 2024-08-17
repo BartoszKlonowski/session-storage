8f12204993aea6b03a718210fd71e7799c8a4f23
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2024-08-17 23:54:40 +0200

    Change SAVE button text according to session already existing

--- app/src/popup/components/Popup.js
+++ app/src/popup/components/Popup.js
@@ -1,21 +1,44 @@
 import * as React from "react";
 import ExpandedSessionListInput from "./ExpandedSessionListInput";
 import ActionButton from "./ActionButton";
+import ExpandedSessionListInput from "./components/ExpandedSessionListInput";
+import ActionButton from "./components/ActionButton";
+import Database from "../engine/Database";
 
 export class Popup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             extensionName: "Session Storage",
+            selectedSession: "",
+            saveButtonTitle: "",
         };
+
+        this.onTextInputChange = this.onTextInputChange.bind(this);
+        this.onSessionsLoad = this.onSessionsLoad.bind(this);
+    }
+
+    onSessionsLoad(sessions) {
+        const buttonTitle = sessions.includes(this.state.selectedSession) ? "OVERWRITE" : "SAVE";
+        this.setState({saveButtonTitle: buttonTitle});
+    }
+
+    onTextInputChange(newValue) {
+        const db = new Database();
+        this.setState({selectedSession: newValue});
+        db.loadSessions(this.onSessionsLoad);
     }
 
     render() {
         return (
             <form id="mainForm">
-                <ExpandedSessionListInput />
+                <ExpandedSessionListInput onTextInputChange={this.onTextInputChange} />
                 <div className="panel-actions">
-                    <ActionButton name="saveButton" text="SAVE" icon="glyphicon glyphicon-floppy-disk" />
+                    <ActionButton
+                        name="saveButton"
+                        text={this.state.saveButtonTitle}
+                        icon="glyphicon glyphicon-floppy-disk"
+                    />
                     <ActionButton name="deleteButton" text="DELETE" icon="glyphicon glyphicon-trash" />
                     <ActionButton name="reopenButton" text="REOPEN" icon="glyphicon glyphicon-refresh" />
                 </div>
