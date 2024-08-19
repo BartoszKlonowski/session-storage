0e0d593a4da190815d946e31a3c85f220bf1bbd1
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2024-08-19 21:51:36 +0200

    Fix the formatting of new import

--- app/src/popup/Main.js
+++ app/src/popup/Main.js
@@ -1,7 +1,7 @@
 import * as logic from "./MainPopupLogic";
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { Popup } from "./components/Popup";
+import {Popup} from "./components/Popup";
 
 if (!browser) {
     var browser = require("webextension-polyfill");
