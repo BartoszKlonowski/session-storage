48df3ea8028ebfa4db036ab0671bbcc665a6e598
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2024-11-02 23:30:14 +0100

    Set the buttons panel width according to longest title

--- app/src/popup/components/Popup.js
+++ app/src/popup/components/Popup.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import ExpandedSessionListInput from "./ExpandedSessionListInput";
 import ActionButton from "./ActionButton";
 import Database from "../../engine/Database";
+import {translate} from "../../engine/i18n";
 
 export class Popup extends React.Component {
     constructor(props) {
@@ -9,9 +10,21 @@ export class Popup extends React.Component {
         this.state = {
             extensionName: "Session Storage",
             saveButtonTitle: "SAVE",
+            buttonContainerWidth: 100,
         };
 
         this.onTextInputChange = this.onTextInputChange.bind(this);
+    }
+
+    componentDidMount() {
+        const buttonsTitles = [
+            translate("overwrite").length,
+            translate("save").length,
+            translate("reopen").length,
+            translate("delete").length,
+        ];
+        const theLongestTitle = Math.max(...buttonsTitles.values());
+        this.setState({buttonContainerWidth: theLongestTitle * 10 + 25});
     }
 
     onTextInputChange(newValue) {
@@ -26,7 +39,7 @@ export class Popup extends React.Component {
         return (
             <form id="mainForm">
                 <ExpandedSessionListInput onTextInputChange={this.onTextInputChange} />
-                <div className="panel-actions">
+                <div className="panel-actions" style={{width: `${this.state.buttonContainerWidth}px`}}>
                     <ActionButton name="saveButton" text={this.state.saveButtonTitle} icon="save" />
                     <ActionButton name="deleteButton" text="DELETE" icon="delete" />
                     <ActionButton name="reopenButton" text="REOPEN" icon="reopen" />
