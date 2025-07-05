2208e0e409906a555653d6538bf866efd1b40d83
Bartosz Klonowski <bartosz-klonowski@cx-code.pl>
2025-07-06 01:05:10 +0200

    Add global.IS_REACT_ACT_ENVIRONMENT for act

--- __tests__/setupTest.js
+++ __tests__/setupTest.js
@@ -2,6 +2,7 @@ if (!global.browser) global.browser = {tabs: {}};
 if (!browser) var browser = {};
 if (!global.browser.i18n) global.browser = {tabs: {}, i18n: {}};
 if (!global.browser.i18n.getUILanguage) global.browser = {tabs: {}, i18n: {getUILanguage: () => "en"}};
+if (!global.IS_REACT_ACT_ENVIRONMENT) global.IS_REACT_ACT_ENVIRONMENT = true;
 
 test("Browser is defined correctly for web extension testing", () => {
     expect(browser).toBeDefined();
